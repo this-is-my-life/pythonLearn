c024c9d40ba463b86360f09c3e02209a48f3c0e7
PMHStudio <pmhstudio.pmh@gmail.com>
2019-07-14 00:51:43 +0900

    연산자

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print('Hello, world!')
+a = 5
+b = 3
+
+print(a + b) # Output -> 8
+print(a - b) # Output -> 2
+print(a * b) # Output -> 15
+print(a / b) # Output -> 1.6666666666667
+print(a // b) # Output -> 1
+print(a % b) # Output -> 2
