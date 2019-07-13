24140768c67202dc4b1c75039c2b28677bc43a00
PMHStudio <pmhstudio.pmh@gmail.com>
2019-07-14 01:04:35 +0900

    I/O

---
+++ index.md
@@ -0,0 +1,9 @@
+# Python Learn
+
+> 연산자
+** 연산자: 거듭제곱
+// 연산자: 나누기인데 반올림한 정수를 리턴
+
+> 입출력
+input()  : 리턴 String, 인수 String - 질문문장
+print()  : 리턴 void, 인수 Any - 출력문장
--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-a = 5
-b = 3
+a = int(input('a> '))
+b = int(input('b> '))
 
-print('a + b', a + b) # Output -> 8
-print('a - b', a - b) # Output -> 2
-print('a * b', a * b) # Output -> 15
-print('a / b', a / b) # Output -> 1.6666666666667
-print('a // b', a // b) # Output -> 1
-print('a %% b', a % b) # Output -> 2
+print('a + b: ', a + b) # Output -> 8
+print('a - b: ', a - b) # Output -> 2
+print('a * b: ', a * b) # Output -> 15
+print('a / b: ', a / b) # Output -> 1.6666666666667
+print('a // b: ', a // b) # Output -> 1
+print('a %% b: ', a % b) # Output -> 2
