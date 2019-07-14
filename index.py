7f502fe485cb9b916d4a7001b17fe8b0ecbbe96c
PMHStudio <pmhstudio.pmh@gmail.com>
2019-07-14 11:27:19 +0900

    more

--- index.py
+++ index.py
@@ -1,11 +1,21 @@
-def cacu():
-  n = int(input('숫자를 입력하세요> '))
-  if n > 0:
-    print(str(n) + '는 양수입니다')
-  elif n < 0:
-    print(str(n) + '는 음수입니다')
-  else:
-    print(str(n) + '은 0입니다')
+import datetime
 
-while True:
-  cacu()
+def cacu():
+  return thisyear - birthyear
+
+def cac1():
+  if cacu() >= 19:
+    return '성인'
+  elif cacu() >= 12:
+    return '청소년'
+  else:
+    return '어린이'
+
+name = input('이름을 입력하세요> ')
+thisyear = datetime.datetime.now().year
+birthyear = int(input('생년을 입력하세요> '))
+
+print('\n' + name + '님 안녕하세요!')
+print('당신의 나이는 만 ' + str(cacu()) + '세입니다')
+print('당신의 나이는 ' + str(cacu() + 1) + '세입니다')
+print('당신은 ' + cac1() + '입니다!')
