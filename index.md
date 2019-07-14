5beedaae9b2fd2c08e354d8d38af6f1a36e3522a
PMHStudio <pmhstudio.pmh@gmail.com>
2019-07-14 10:53:47 +0900

    defs

--- index.md
+++ index.md
@@ -6,7 +6,10 @@
 
 > 입출력
 input()  : 리턴 String, 인수 String - 질문문장
-print()  : 리턴 void, 인수 Any - 출력문장
+print()  : 리턴 void, 인수 String - 출력문장
+
+> 함수
+def 함수이름(인수) : 함수 선언 - 사용되는 곳보다 위에 위치해야 함
 
 # 질문
 1. python에서는 객체를 어떻게 쓰지?
--- index.py
+++ index.py
@@ -1,2 +1,12 @@
+import datetime
+
+def cacu():
+  return thisyear - birthyear
+
 name = input('이름을 입력하세요> ')
+thisyear = datetime.datetime.now().year
+birthyear = int(input('생년을 입력하세요> '))
+
 print(name + '님 안녕하세요!')
+print('당신의 나이는 만 ' + str(cacu()) + '세입니다')
+print('당신의 나이는 ' + str(cacu() + 1) + '세입니다')
