ddf3b80aada781a755c56fb2344ceb510b5488b0
PMHStudio <pmhstudio.pmh@gmail.com>
2019-07-14 11:03:10 +0900

    l & S caculator

--- index.py
+++ index.py
@@ -1,12 +1,8 @@
-import datetime
+import math
 
-def cacu():
-  return thisyear - birthyear
 
-name = input('이름을 입력하세요> ')
-thisyear = datetime.datetime.now().year
-birthyear = int(input('생년을 입력하세요> '))
+def cacu(r):
+  print('l: ' + str(math.pi * r ** 2) + 'cm')
+  print('S: ' + str(math.pi * 2 * r) + '㎠')
 
-print(name + '님 안녕하세요!')
-print('당신의 나이는 만 ' + str(cacu()) + '세입니다')
-print('당신의 나이는 ' + str(cacu() + 1) + '세입니다')
+cacu(int(input('반지름을 입력하세요 (cm)> ')))
